840aac1a41a1a23424af11b4f1267f989c1e3677
Casey Kalinowski <MediocreFred@users.noreply.github.com>
2020-03-11 22:17:29 -0400

    DONE

--- thing.py
+++ thing.py
@@ -1,16 +1,16 @@
-import mysql.connector
+import mysql.connector, csv
 
 
 class Player:
     def __init__(self, raw_data):
-        self.attributes = {'first_name': raw_data[0], 'last_name': raw_data[1], 'Team Name': raw_data[60],
+        self.attributes = {'First Name': raw_data[0], 'Last Name': raw_data[1], 'Team Name': raw_data[60],
                            'Team Nickname': raw_data[61], 'Aggression': raw_data[9],
                            'Bravery': raw_data[10], 'Determination': 0, 'Teamplayer': 0,
                            'Leadership': 0, 'Temperament': 0, 'Professionalism': 0,
                            'Mental Toughness': raw_data[16], 'Goalie Stamina': raw_data[17], 'Acceleration': raw_data[18],
                            'Agility': raw_data[19], 'Balance': raw_data[20], 'Speed': raw_data[21], 'Stamina': raw_data[22],
                            'Strength': raw_data[23], 'Fighting': raw_data[24], 'Screening': raw_data[25],
-                           'Getting Open': raw_data[26], 'Passing': raw_data[26], 'Puck Handling': raw_data[28],
+                           'Getting Open': raw_data[26], 'Passing': raw_data[27], 'Puck Handling': raw_data[28],
                            'Shooting Accuracy': raw_data[29], 'Shooting Range': raw_data[30],
                            'Offensive Read': raw_data[31], 'Checking': raw_data[32], 'Faceoffs': raw_data[33],
                            'Hitting': raw_data[34], 'Positioning': raw_data[35], 'Shot Blocking': raw_data[36],
@@ -18,7 +18,7 @@ class Player:
                            'G Passing': raw_data[40], 'G Pokecheck': raw_data[41], 'Blocker': raw_data[42],
                            'Glove': raw_data[43], 'Rebound': raw_data[44], 'Recovery': raw_data[45],
                            'G Puckhandling': raw_data[46], 'Low Shots': raw_data[47], 'G Skating': raw_data[48],
-                           'Reflexes': raw_data[49], 'Skating': raw_data[50]}
+                           'Reflexes': raw_data[49]}
         self.attributes['TPE'] = self.calculate_tpe()
 
     def calculate_tpe(self):
@@ -87,13 +87,19 @@ def main():
 
     myresult = mycursor.fetchall()
 
-    with open('player.txt', 'w+') as file:
-        count = 0
-        for x in myresult:
-            player = Player(x)
-            count += 1
-            file.write(str(player.attributes) + '\n')
-
+    player_list = list()
+    count = 0
+    for x in myresult:
+        player = Player(x)
+        player_list.append(player.attributes)
+        count += 1
+    csv_file = 'fhm6_all_players.csv'
+    csv_columns = player_list[0].keys()
+    with open(csv_file, 'w+', encoding='utf-8-sig', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for data in player_list:
+            writer.writerow(data)
     print(count)
 
 main()
